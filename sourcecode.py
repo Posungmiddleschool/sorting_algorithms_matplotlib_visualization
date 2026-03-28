8a8886b0e106135e2a2404d096c1bab2dd53c555
Posungmiddleschool <hms.guest00@gmail.com>
2026-03-28 20:52:56 +0900

    Implement sorting algorithm visualization with matplotlib
    Added interactive visualization for sorting algorithm.

--- sourcecode.py
+++ sourcecode.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
+plt.ion()
+fig , ax = plt.subplots(figsize=(10,6))
+result = []
 my_array = []
-x_axis = []
-for i in range(100):
-  x_axis.append(i)
 for i in range(100):
   my_array.append(random.randint(0,100))
-
+def visualization():
+  plt.title("Sorting Algorithm Visualization")
+  plt.xlabel('x')
+  plt.ylabel('y')
 def sortlist(arr):
+  global result
   if len(arr) <= 1:
     return arr
   pivot = arr[len(arr) // 2 ]
@@ -22,12 +26,18 @@ def sortlist(arr):
       mid_list.append(values)
     else:
       right_list.append(values)
-  return sortlist(left_list) + mid_list + sortlist(right_list)
+  result =  sortlist(left_list) + mid_list + sortlist(right_list)
+  ax.clear()
+  ax.bar(range(len(result)), result, color='salmon')
+  plt.pause(0.05)
+  return result
 
-plt.title("Sorting Algorithm Visualization")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.bar(x_axis, my_array)
+sorted_list=sortlist(my_array)
+plt.ioff()
+ax.clear()
 plt.show()
-sortlist(my_array)
-plt.show()
+
+
+
+
+
